0a69e43c473a737e44cb716b22dbf43179af5c82
Xuan <xianxiumei0506@gmail.com>
2018-07-11 01:31:39 -0700

    trying to apply pylint for travis ci

--- unit_testing.sh
+++ unit_testing.sh
@@ -1,4 +1,7 @@
 #/bin/sh
 source $HOME/build/neuropoly/spinalcordtoolbox/python/bin/activate
 export PYTHONPATH=$PYTHONPATH:$SCT_DIR/scripts
-pytest -v --cov-append --cov=spinalcordtoolbox --cov=scripts unit_testing/*
+# pytest -v --cov-append --cov=spinalcordtoolbox --cov=scripts unit_testing/*
+
+pylint --py3k spinalcordtoolbox/*
+pylint --py3k scripts/*

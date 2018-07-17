285f4aa6e4b171e371829f4cc39dab23c4b8c0a1
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-07-17 00:33:38 -0400

    added test for process_seg.compute_csa

--- unit_testing/test_process_seg.py
+++ unit_testing/test_process_seg.py
@@ -12,6 +12,7 @@ from spinalcordtoolbox import process_seg
 @pytest.fixture(scope="session")
 def dummy_segmentation():
     """Create a dummy image with a circle or ones running from top to bottom in the 3rd dimension"""
+    # TODO: rotate the image to make sure the CSA properly accounts for the angle correction
     nx, ny, nz = 20, 20, 20  # image dimension
     fname_seg = 'dummy_segmentation.nii.gz'  # output seg
     data = np.random.random((nx, ny, nz))
@@ -27,9 +28,8 @@ def dummy_segmentation():
 
 # noinspection 801,PyShadowingNames
 def test_extract_centerline(dummy_segmentation):
-    """Test generation of centerline"""
-    process_seg.extract_centerline(dummy_segmentation, 0, verbose=0, algo_fitting='hanning', type_window='hanning',
-                                   window_length=80, use_phys_coord=True, file_out='centerline')
+    """Test extraction of centerline from input segmentation"""
+    process_seg.extract_centerline(dummy_segmentation, 0, file_out='centerline')
     # open created csv file
     centerline_out = []
     with open('centerline.csv', 'rb') as f:
@@ -40,3 +40,18 @@ def test_extract_centerline(dummy_segmentation):
     # build ground-truth centerline
     centerline_true = [[i, 9, 10] for i in range(20)]
     assert centerline_out == centerline_true
+
+
+# noinspection 801,PyShadowingNames
+def test_compute_csa(dummy_segmentation):
+    """Test computation of cross-sectional area from input segmentation"""
+    process_seg.compute_csa(dummy_segmentation, 1, 1, 1, '', '', fname_vertebral_labeling='', perslice=0, perlevel=0,
+                            algo_fitting='hanning', type_window='hanning', window_length=10, angle_correction=True,
+                            use_phys_coord=True, fname_out='csa.csv')
+    # open created csv file
+    with open('csa.csv', 'rb') as f:
+        reader = csv.reader(f)
+        reader.next()  # skip header
+        csa_out, angle_out = reader.next()[2:4]
+    assert csa_out == '29.0'
+    assert angle_out == '0.0'

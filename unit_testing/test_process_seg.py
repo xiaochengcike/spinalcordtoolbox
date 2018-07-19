c633ee64c1d860b2534030a04f592ce04776ad54
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-07-19 14:53:16 -0400

    image is now rotated for testing, raising a failed test on compute_shape

--- unit_testing/test_process_seg.py
+++ unit_testing/test_process_seg.py
@@ -6,23 +6,31 @@ import numpy as np
 import nibabel as nib
 import csv
 import pytest
+from skimage.transform import rotate
 from spinalcordtoolbox import process_seg
 
 
 @pytest.fixture(scope="session")
 def dummy_segmentation():
-    """Create a dummy image with a circle or ones running from top to bottom in the 3rd dimension"""
-    # TODO: rotate the image to make sure the CSA properly accounts for the angle correction
+    """Create a dummy image with a ellipse or ones running from top to bottom in the 3rd dimension, and rotate the image
+    to make sure that compute_csa and compute_shape properly estimate the centerline angle."""
     nx, ny, nz = 20, 20, 20  # image dimension
     fname_seg = 'dummy_segmentation.nii.gz'  # output seg
     data = np.random.random((nx, ny, nz))
     xx, yy = np.mgrid[:nx, :ny]
-    # loop across slices and add an ellipse of axis lenght a and b
+    # loop across slices and add an ellipse of axis length a and b
     a, b = 5.0, 3.0  # diameter of ellipse
     for iz in range(nz):
         data[:, :, iz] = (((xx - nx / 2) / a) ** 2 + ((yy - ny / 2) / b) ** 2 <= 1) * 1
+    # swap x-z axes (to make a rotation within y-z plane)
+    data_swap = data.swapaxes(0, 2)
+    # rotate by 15 deg, and re-grid using linear interpolation (more accurate for csa and shape measures)
+    data_swap_rot = rotate(data_swap, 15, resize=False, center=None, order=1, mode='constant', cval=0, clip=True,
+                           preserve_range=False)
+    # swap back
+    data_rot = data_swap_rot.swapaxes(0, 2)
     xform = np.eye(4)
-    img = nib.nifti1.Nifti1Image(data, xform)
+    img = nib.nifti1.Nifti1Image(data_rot, xform)
     nib.save(img, fname_seg)
     return fname_seg
 
@@ -46,7 +54,7 @@ def test_extract_centerline(dummy_segmentation):
 # noinspection 801,PyShadowingNames
 def test_compute_csa(dummy_segmentation):
     """Test computation of cross-sectional area from input segmentation"""
-    process_seg.compute_csa(dummy_segmentation, 1, 1, 1, '', '', fname_vertebral_labeling='', perslice=0, perlevel=0,
+    process_seg.compute_csa(dummy_segmentation, 1, 1, 1, '5:15', '', fname_vertebral_labeling='', perslice=0, perlevel=0,
                             algo_fitting='hanning', type_window='hanning', window_length=10, angle_correction=True,
                             use_phys_coord=True, file_out='csa')
     # open created csv file
@@ -54,14 +62,14 @@ def test_compute_csa(dummy_segmentation):
         reader = csv.reader(f)
         reader.next()  # skip header
         csa_out, angle_out = reader.next()[2:4]
-    assert csa_out == '45.0'
-    assert angle_out == '0.0'
+    assert float(csa_out) == pytest.approx(44.989, abs=1e-3)
+    assert float(angle_out) == pytest.approx(15.009, abs=1e-3)
 
 
 # noinspection 801,PyShadowingNames
 def test_compute_shape(dummy_segmentation):
     """Test computation of cross-sectional area from input segmentation"""
-    process_seg.compute_shape_from_file(dummy_segmentation, slices='', vert_levels='', fname_vert_levels='', perslice=0,
+    process_seg.compute_shape_from_file(dummy_segmentation, slices='5:15', vert_levels='', fname_vert_levels='', perslice=0,
                                         perlevel=0, file_out='shape', overwrite=0, remove_temp_files=1, verbose=1)
     # open created csv file
     with open('shape.csv', 'rb') as f:
@@ -78,6 +86,3 @@ def test_compute_shape(dummy_segmentation):
     assert solidity == pytest.approx(0.865737742, abs=1e-3)
     assert orientation == pytest.approx(0.230464909, abs=1e-3)
     assert symmetry == pytest.approx(0.997869087, abs=1e-3)
-
-    # TODO: continue integrity testing
-    # TODO: test on an ellipsoid image, because the orientation field does not make any sense on a cylindrical image

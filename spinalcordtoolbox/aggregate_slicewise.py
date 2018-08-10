d7c8c1643464f92d9a89c3c5b99bf0f3c4932796
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-08-10 16:36:07 -0400

    started refactoring of aggregage_slicewise

--- spinalcordtoolbox/aggregate_slicewise.py
+++ spinalcordtoolbox/aggregate_slicewise.py
@@ -26,8 +26,8 @@ def aggregate_per_slice_or_level(metrics, slices=[], levels=[], perslice=True, p
     # TODO: remove the csv writing part from this function and create wrapper average_per_slice_or_level_to_file().
     # TODO: check last dimension is S-I
     # TODO: move the parse_num_list to the front-end
-    agg_metrics = dict((metric, ()) for metric in metrics.keys())
-
+    # agg_metrics = dict((metric, ()) for metric in metrics.keys())
+    agg_metrics = dict((metric, dict()) for metric in metrics.keys())
     # # nz = len(metrics[0])  # retrieve number of slices from the first metric (assuming they all have the same shape)
     # if slices:
     #     # if user specified slices of interest, convert to comma-separated string: '2:5,6' -> '2,3,4,5,6'
@@ -47,65 +47,60 @@ def aggregate_per_slice_or_level(metrics, slices=[], levels=[], perslice=True, p
     # if user selected vertebral levels
     if levels:
         im_vert_level.change_orientation("RPI")  # last dim should be k
-
+        slicegroups = [tuple(get_slices_from_vertebral_levels(im_vert_level, level)) for level in levels]
         if perlevel:
-            # XXX
-            a=1
+            # slicegroups = [(0, 1, 2), (3, 4, 5), (6, 7, 8)]
+            # vertgroups = [(2,), (3,), (4,)]
+            vertgroups = [tuple([level]) for level in levels]
         # output aggregate metrics across levels
         else:
-            # get slices corresponding to levels
-            # TODO: the thing below is ugly-- there must be a better way
-            slicegroups = [get_slices_from_vertebral_levels(im_vert_level, level) for level in levels]
-            # slices = [val for sublist in list_slices for val in sublist]  # flatten list_slices
-
-        # Re-define slices_of_interest according to the vertebral levels selected by user
-        list_levels = parse_num_list(levels)
-        slicegroups = []
-        vertgroups = [str(i) for i in list_levels]
-        # for each level, find the matching slices and group them
-        for level in list_levels:
-            list_slices = get_slices_from_vertebral_levels(im_vert_level, level)
-            list_slices.reverse()
-            slicegroups.append(','.join([str(i) for i in list_slices]))
-        # if user does not want to output metric per vert level, create a single element in vertgroups
-        if not perlevel:
-            # ['2', '3', '4'] -> ['2,3,4']
-            vertgroups = [','.join(vertgroups)]
-            slicegroups = [','.join(slicegroups)]
+            # slicegroups = [(0, 1, 2, 3, 4, 5, 6, 7, 8)]
+            slicegroups = [tuple([val for sublist in slicegroups for val in sublist])]  # flatten list_slices
+            # vertgroups = [(2, 3, 4)]
+            vertgroups = [tuple([level for level in levels])]
+        #
+        # # Re-define slices_of_interest according to the vertebral levels selected by user
+        # list_levels = parse_num_list(levels)
+        # slicegroups = []
+        # vertgroups = [str(i) for i in list_levels]
+        # # for each level, find the matching slices and group them
+        # for level in list_levels:
+        #     list_slices = get_slices_from_vertebral_levels(im_vert_level, level)
+        #     list_slices.reverse()
+        #     slicegroups.append(','.join([str(i) for i in list_slices]))
+        # # if user does not want to output metric per vert level, create a single element in vertgroups
+        # if not perlevel:
+        #     # ['2', '3', '4'] -> ['2,3,4']
+        #     vertgroups = [','.join(vertgroups)]
+        #     slicegroups = [','.join(slicegroups)]
 
     # loop across slice group
     for slicegroup in slicegroups:
         try:
-            # convert list of strings into list of int to use as index
-            ind_slicegroup = [int(i) for i in slicegroup.split(',')]
-            if levels:
-                vertgroup = vertgroups[slicegroups.index(slicegroup)]
-            else:
-                vertgroup = ''
-            # average metrics within slicegroup
-            # TODO: ADD STD
-            # change "," for ";" otherwise it will be parsed by the CSV format
-            # TODO: instead of having a long list of ;-separated numbers, it would be nicer to separate long number
-            # TODO (cont.) suites with ":". E.g.: '1,2,3,4,5' -> '1:5'. See #1932
-            slicegroup = slicegroup.replace(",", ";")
-            vertgroup = vertgroup.replace(",", ";")
-            # encode slice and levels
+            # # convert list of strings into list of int to use as index
+            # ind_slicegroup = [int(i) for i in slicegroup.split(',')]
+            # if levels:
+            #     vertgroup = vertgroups[slicegroups.index(slicegroup)]
+            # else:
+            #     vertgroup = ''
+            # # average metrics within slicegroup
+            # # TODO: ADD STD
+            # # change "," for ";" otherwise it will be parsed by the CSV format
+            # # TODO: instead of having a long list of ;-separated numbers, it would be nicer to separate long number
+            # # TODO (cont.) suites with ":". E.g.: '1,2,3,4,5' -> '1:5'. See #1932
+            # slicegroup = slicegroup.replace(",", ";")
+            # vertgroup = vertgroup.replace(",", ";")
+            # # encode slice and levels
+
+            # add level info
+            agg_metrics['VertLevel'][slicegroup] = vertgroups[slicegroups.index(slicegroup)]
 
             for metric in metrics.keys():
-                metric_data = metrics[metric]
-                level_data = [metric_data[idx_slice] for idx_slice in idx_slices if 0 <= idx_slice < len(metric_data)]
-                if not level_data:
-                    break
-                agg_metrics[metric] = dict((name, func(level_data)) for (name, func) in group_funcs)
+                metric_data = [metrics[metric][slice] for slice in slicegroup]
+                agg_metrics[metric][slicegroup] = dict((name, func(metric_data)) for (name, func) in group_funcs)
         except IndexError:
-            # the slice request is out of the range of the image
-            sct.log.warning('The slice(s) requested is out of the range of the image')
-
+            sct.log.warning('The slice request is out of the range of the image')
     return agg_metrics
-            # agg_metrics[metric] = dict((name, func(level_data)) for (name, func) in group_funcs)
-            # build csv file
-            # file_results.write(','.join([slicegroup, vertgroup] + [str(np.mean(i[ind_slicegroup])) for i in metrics])
-            #                    + '\n')
 #
 #     # Create output csv file
 #     fname_out = file_out + '.csv'
